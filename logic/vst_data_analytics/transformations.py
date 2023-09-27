0b433e2be3e72e3003858254e85866229e382823
Patrick Börzel <patrick.boerzel@qunis.de>
2023-09-27 13:30:17 +0200

    Chnage dataframe from spark to pandas

--- logic/vst_data_analytics/transformations.py
+++ logic/vst_data_analytics/transformations.py
@@ -1,16 +1,14 @@
 from functools import reduce
 
-from pyspark.sql import DataFrame
+import pandas as pd
 import numpy as np
 
-def rename_columns(df: DataFrame, mapping: dict[str, str]) -> DataFrame:
-    def rename_column(df: DataFrame, col_definition: tuple[str, str]) -> DataFrame:
-        return df.withColumnRenamed(col_definition[0], col_definition[1])
 
-    return reduce(rename_column, mapping.items(), df)
+def rename_columns(df: pd.DataFrame, mapping: dict[str, str]) -> pd.DataFrame:
+    return df.rename(columns=mapping)
 
 
-def replace_nan(df: DataFrame) -> DataFrame:
+def replace_nan(df: pd.DataFrame) -> pd.DataFrame:
     df.replace("None", np.NaN).replace("nan", np.NaN)
 
     return df
